12c9aa9f5b73e74c50992c36cf09eb75559cda1a
John Trinh <john.trinh2704@gmail.com>
2025-07-05 16:31:19 +0700

    init source and theme

---
+++ jest.config.ts
@@ -0,0 +1,21 @@
+import type { Config } from "jest";
+import nextJest from "next/jest.js";
+
+const createJestConfig = nextJest({
+  dir: "./",
+});
+
+// Add any custom config to be passed to Jest
+const config: Config = {
+  coverageProvider: "v8",
+  testEnvironment: "jsdom",
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
+  moduleNameMapper: {
+    "^@/(.*)$": "<rootDir>/src/$1",
+  },
+  // Load environment variables in tests
+  setupFiles: ["<rootDir>/jest.env.setup.ts"],
+};
+
+// createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
+export default createJestConfig(config);
---
+++ jest.setup.ts
@@ -0,0 +1,4 @@
+import "@testing-library/jest-dom";
+
+// Enable fetch mocking in Jest
+global.fetch = jest.fn();
